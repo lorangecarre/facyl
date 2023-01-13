747e3176cba0477b2b8c3eab9588c4a38767d7fe
Louison <72253677+developpementorangecarre@users.noreply.github.com>
2023-01-13 11:44:42 +0100

    File d'Ariane

--- page-contact.php
+++ page-contact.php
@@ -9,6 +9,11 @@ get_header(); ?>
 
 <main class="site-main">
   <section class="contact">
+   <?php
+     if ( function_exists('yoast_breadcrumb') ) {
+       yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
+    }
+  ?>
     <h1>  <?php the_title(); ?> </h1>
     <?php the_content(); ?>
   </section>
