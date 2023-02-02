5b3cd9086d010e472d42aa4d45a66161c6c4b490
samuelweissgerber <samuel@orangecarre.fr>
2023-02-02 09:14:06 +0100

    fix: modification accéssibilité page formulaire facyl par rapport au retour

--- inc/includes.php
+++ inc/includes.php
@@ -41,4 +41,5 @@ add_filter( 'wpcf7_autop_or_not', '__return_false' );
 add_action( 'customize_register', 'customizer_footer' );
 // add_action( 'acf/field_group/admin_enqueue_scripts', 'my_acf_field_group_admin_enqueue_scripts' );
 add_action( 'init', 'custom_wp_remove_global_css' );
-
+remove_action( 'template_redirect', 'wpcf7_cleanup_upload_files', 20 );
+add_action( 'template_redirect', 'custom_wpcf7_cleanup_upload_files', 20, 0 );
